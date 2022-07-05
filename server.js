c5b61d2ce71366ae3b8b6cb043eaaa4550597768
marcusloy77 <marcus.loy.77@gmail.com>
2022-07-06 03:14:57 +1000

    heroku deploy

--- server.js
+++ server.js
@@ -23,3 +23,12 @@ app.use('/api/sessions', sessionsController)
 app.use('/api/users', usersController)
 app.use('/api/friends', friendsController)
 app.use('/api/messages', messagesController)
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
