83880b520896dbff198d91516f8f96980e7f68bd
marcusloy77 <marcus.loy.77@gmail.com>
2022-08-01 19:19:22 +1000

    deployment basic guide

--- db/schema.sql
+++ db/schema.sql
@@ -10,14 +10,4 @@ CREATE TABLE users(
   password_digest TEXT
 );
 
-CREATE TABLE friends_list_{user_id}(
-  id SERIAL PRIMARY KEY,
-  friend_id TEXT
-);
-
-CREATE TABLE conversation_{user_id1}_{user_id2}(
-  id SERIAL PRIMARY KEY, 
-  user_id_1 INTEGER, 
-  user_id_2 INTEGER
-);
 
